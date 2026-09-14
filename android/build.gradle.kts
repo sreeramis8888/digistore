1c17496bd0410bd5afe6b2e22757ec6f9efbc0e1
sreeramis8888 <128227002+sreeramis8888@users.noreply.github.com>
2026-09-14 11:13:37 +0530

    feat: update dependencies in pubspec.lock and enhance resolution strategy in build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,20 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    configurations.all {
+        resolutionStrategy {
+            force("androidx.datastore:datastore:1.1.7")
+            force("androidx.datastore:datastore-core:1.1.7")
+            force("androidx.datastore:datastore-preferences:1.1.7")
+            force("androidx.datastore:datastore-preferences-core:1.1.7")
+            force("androidx.datastore:datastore-android:1.1.7")
+            force("androidx.datastore:datastore-core-android:1.1.7")
+            force("androidx.datastore:datastore-preferences-android:1.1.7")
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
